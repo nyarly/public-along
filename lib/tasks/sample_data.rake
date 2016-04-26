95404258a0c202bf7378fca0a34ebb31c771e9a8
Patricia Ho <pho@opentable.com>
2016-04-26 14:50:58 -0700

    Create basic Employee model and methods to create an AD user via ldap

---
+++ lib/tasks/sample_data.rake
@@ -0,0 +1,44 @@
+namespace :db do
+  namespace :sample_data do
+    task :load => :environment do
+      2.times do
+        create_basic_new_hire(DateTime.now)
+      end
+  end
+end
+
+def create_basic_new_hire(hire_date)
+  Employee.create!(
+        email: nil, # Regular new hires don't have an email
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        workday_username: Faker::Number.number(10),
+        employee_id: Faker::Number.number(10),
+        country: Faker::Address.country_code,
+        hire_date: hire_date,
+        contract_end_date: nil,
+        termination_date: nil,
+        job_family_id: Faker::Number.number(10),
+        job_family: Faker::Lorem.words(3).join(' '),
+        job_profile_id: Faker::Number.number(10),
+        job_profile: Faker::Name.title,
+        business_title: Faker::Name.title,
+        employee_type: 'Regular',
+        contingent_worker_id: nil,
+        contingent_worker_type: nil,
+        location_type: 'HQ',
+        location: 'OT_San_Francisco',
+        manager_id: nil,
+        cost_center: nil,
+        cost_center_id: nil,
+        personal_mobile_phone: Faker::PhoneNumber.phone_number,
+        office_phone: Faker::PhoneNumber.phone_number,
+        home_address_1: nil,
+        home_address_2: nil,
+        home_city: nil,
+        home_state: nil,
+        home_zip: nil,
+        image_code: nil,
+      )
+    end
+end
--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,15 +1,50 @@
 require 'rails_helper'
 
 describe Employee, type: :model do
-  let(:employee) { FactoryGirl.build(:employee) }
+  let(:employee) { FactoryGirl.build(:employee,
+    first_name: "Bob",
+    last_name: "Barker"
+  ) }
 
-  it "should meet validations" do 
+  it "should meet validations" do
     expect(employee).to be_valid
 
     expect(employee).to_not allow_value(nil).for(:first_name)
     expect(employee).to_not allow_value(nil).for(:last_name)
+  end
 
-    expect(employee).to_not allow_value(nil).for(:email)
-    expect(employee).to validate_uniqueness_of(:email).case_insensitive
+  it "should create a cn" do
+    expect(employee.cn).to eq("Bob Barker")
+  end
+
+  it "should create a dn" do
+    expect(employee.dn).to eq("cn=Bob Barker,cn=Users,dc=ottest,dc=opentable,dc=com")
+  end
+
+  it "should create a sAMAccountName" do
+    expect(employee.sAMAccountName).to eq("bbarker")
+  end
+
+  it "should create attr hash" do
+    expect(employee.attrs).to eq(
+      {
+        cn: "Bob Barker",
+        objectclass: ["top", "person", "organizationalPerson", "user"],
+        givenName: "Bob",
+        sn: "Barker",
+        sAMAccountName: "bbarker",
+        mail: "bbarker@opentable.com",
+        unicodePwd: "\"Password!12345\"".encode(Encoding::UTF_16LE).force_encoding(Encoding::ASCII_8BIT)
+      }
+    )
+  end
+
+  xit "should create an ldap connection" do
+  end
+
+  xit "should create an ad user" do
+  end
+
+  xit "should modify the user to be a normal user" do
   end
 end
