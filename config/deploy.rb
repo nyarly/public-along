38f242ce6d438231ab53931643c8975fbf036330
Patricia Ho <pho@opentable.com>
2016-05-04 18:14:15 -0700

    change deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 server '172.16.5.2', roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:opentable/workday-integration.git'
-set :application,     'workday-integration'
+set :application,     'workday_integration'
 set :user,            'workday'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
@@ -13,7 +13,7 @@ set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :copy
 set :deploy_to,       "/var/www/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_bind,       "tcp://localhost:3000"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
@@ -42,7 +42,7 @@ set :branch,        :deploy_setup
 # set :keep_releases, 5
 
 ## Linked Files & Directories (Default None):
-set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml config/ldap.yml}
 # set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
