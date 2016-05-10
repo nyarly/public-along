e1adf5964cefcc48c36f5af06626662377f0fc58
Patricia Ho <pho@opentable.com>
2016-05-10 14:38:56 -0700

    complete 1165

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -11,11 +11,55 @@ class Employee < ActiveRecord::Base
   end
 
   def dn
-    "cn=#{cn},cn=Users,dc=ottest,dc=opentable,dc=com" #TODO needs correct ou structure
+    "cn=#{cn}," + ou + BASE
   end
 
-  def plain_text_password
-    "Password!12345" #TODO What temp password are we supposed to use
+  def ou
+    match = OUS.select { |k,v|
+      v[:department].include?(cost_center) && v[:country].include?(country)
+    }
+
+    if match.length == 1
+      match.keys[0]
+    else
+      puts "WARNING: could not find an exact ou match for #{first_name} #{last_name}; placed in default ou"
+      return ""
+    end
+  end
+
+  def encode_password
+    #TODO Replace this with a randomized password that gets sent to the new hire via email/text/???
+    "\"123Opentable\"".encode(Encoding::UTF_16LE).force_encoding(Encoding::ASCII_8BIT)
+  end
+
+  def generated_email
+    #TODO Some contingent workers get emails and others don't
+    if sAMAccountName.present? && contingent_worker_type.blank?
+      sAMAccountName + "@opentable.com"
+    else
+      nil
+    end
+  end
+
+  def generated_account_expires
+    #TODO refactor windows FILETIME struct conversion
+    if termination_date.present?
+      return DateTimeHelper::FileTime.wtime(termination_date)
+    elsif contract_end_date.present?
+      DateTimeHelper::FileTime.wtime(contract_end_date)
+    else
+      nil
+    end
+  end
+
+  def generated_address
+    if home_address_1.present? && home_address_2.present?
+      home_address_1 + ", " + home_address_2
+    elsif home_address_1.present?
+      home_address_1
+    else
+      nil
+    end
   end
 
   def attrs
@@ -25,15 +69,24 @@ class Employee < ActiveRecord::Base
       givenName: first_name,
       sn: last_name,
       sAMAccountName: sAMAccountName,
-      mail: sAMAccountName + "@opentable.com",
-      unicodePwd: "\"#{plain_text_password}\"".encode(Encoding::UTF_16LE).force_encoding(Encoding::ASCII_8BIT)
-    }
-    # {
-    #   cn: cn,
-    #   objectclass: ["top", "person", "organizationalPerson", "user"],
-    #   givenName: first_name,
-    #   sn: last_name,
-    #   unicodePwd: "\"#{plain_text_password}\"".encode(Encoding::UTF_16LE).force_encoding(Encoding::ASCII_8BIT)
-    # }
+      mail: generated_email,
+      unicodePwd: encode_password,
+      workdayUsername: workday_username,
+      co: country,
+      accountExpires: generated_account_expires,
+      title: business_title,
+      description: business_title,
+      employeeType: employee_type,
+      physicalDeliveryOfficeName: location,
+      department: cost_center,
+      employeeID: employee_id,
+      mobile: personal_mobile_phone,
+      telephoneNumber: office_phone,
+      streetAddress: generated_address,
+      l: home_city,
+      st: home_state,
+      postalCode: home_zip,
+      thumbnailPhoto: image_code
+    }.delete_if { |k,v| v.blank? } # AD does not accept nil or empty strings
   end
 end
--- app/services/active_directory_service.rb
+++ app/services/active_directory_service.rb
@@ -3,7 +3,7 @@ class ActiveDirectoryService
 
   def initialize
     @ldap ||= begin
-      puts "new connection"
+      puts "Starting new connection..."
       l = Net::LDAP.new
       l.host = Rails.application.secrets.ad_host
       l.port = 636
@@ -13,24 +13,32 @@ class ActiveDirectoryService
       l
     end
   end
-  # create new ad users from list of employees
 
   def create_disabled(employees)
     employees.each do |e|
       if assign_sAMAccountName(e)
+        puts "creating #{e.first_name} #{e.last_name}"
+        #TODO create a general validation for contingent workers
+        if e.contingent_worker_type.present? && e.contract_end_date.blank?
+          puts "WARNING: #{e.first_name} #{e.last_name} is a contingent worker and needs a contract_end_date. A disabled Active Directory user has been created, but will not be enabled until the contract_end_date is provided"
+        end
         ldap.add(dn: e.dn, attributes: e.attrs)
         puts ldap.get_operation_result
       else
-        puts "could not find a suitable sAMAccountName for #{e.first_name} #{e.last_name}: Must create AD account manually"
+        puts "ERROR: could not find a suitable sAMAccountName for #{e.first_name} #{e.last_name}: Must create AD account manually"
       end
     end
   end
-  # modify users from list of employees
 
   def make_normal(employees)
     employees.each do |e|
-      ldap.replace_attribute(e.dn, :userAccountControl, "512")
-      puts ldap.get_operation_result
+      #TODO create a general validation for contingent workers
+      if e.contingent_worker_type.present? && e.contract_end_date.blank?
+        puts "ERROR: #{e.first_name} #{e.last_name} is a contingent worker and needs a contract_end_date. Account not activated."
+      else
+        ldap.replace_attribute(e.dn, :userAccountControl, "512")
+        puts ldap.get_operation_result
+      end
     end
   end
 
@@ -39,13 +47,13 @@ class ActiveDirectoryService
     last = employee.last_name
 
     sam_options = [
-      (first[0,1] + last).downcase, 
+      (first[0,1] + last).downcase,
       (first + last[0,1]).downcase,
       (first + last).downcase
     ]
 
     sam_options.each do |sam|
-      if find_entry("sAMAccountName", sam).empty?
+      if find_entry("sAMAccountName", sam).blank?
         employee.sAMAccountName = sam
         break
       end
