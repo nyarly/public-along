f76b87a7521a8b86830f57fe8896234ca4671544
Patricia Ho <pho@opentable.com>
2016-05-11 10:23:53 -0700

    clean up deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,3 @@
-# server '172.16.5.2', roles: [:web, :app, :db], primary: true
-
 set :repo_url,        'git@github.com:opentable/workday-integration.git'
 set :application,     'workday_integration'
 set :user,            'workday'
@@ -24,20 +22,11 @@ set :puma_init_active_record, true
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.0'
 
-# in case you want to set ruby version from the file:
-# set :rbenv_ruby, File.read('.ruby-version').strip
-
 set :rbenv_custom_path , "/usr/local/rbenv"
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_custom_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_custom_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
-
-## Defaults:
-# set :scm,           :git
-set :branch,        :deploy_setup
-# set :format,        :pretty
-# set :log_level,     :debug
-# set :keep_releases, 5
+set :tmp_dir, "/home/workday/tmp"
 
 ## Linked Files & Directories (Default None):
 set :linked_files, %w{config/database.yml config/secrets.yml config/ldap.yml}
@@ -91,55 +80,3 @@ end
 # ps aux | grep puma    # Get puma pid
 # kill -s SIGUSR2 pid   # Restart puma
 # kill -s SIGTERM pid   # Stop puma
-
-# Original deploy.rb from cap install
-
-# # config valid only for current version of Capistrano
-# lock '3.5.0'
-
-# set :application, 'my_app_name'
-# set :repo_url, 'git@example.com:me/my_repo.git'
-
-# # Default branch is :master
-# # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# # Default deploy_to directory is /var/www/my_app_name
-# # set :deploy_to, '/var/www/my_app_name'
-
-# # Default value for :scm is :git
-# # set :scm, :git
-
-# # Default value for :format is :airbrussh.
-# # set :format, :airbrussh
-
-# # You can configure the Airbrussh format using :format_options.
-# # These are the defaults.
-# # set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
-
-# # Default value for :pty is false
-# # set :pty, true
-
-# # Default value for :linked_files is []
-# # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-
-# # Default value for linked_dirs is []
-# # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
-
-# # Default value for default_env is {}
-# # set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# # Default value for keep_releases is 5
-# # set :keep_releases, 5
-
-# namespace :deploy do
-
-#   after :restart, :clear_cache do
-#     on roles(:web), in: :groups, limit: 3, wait: 10 do
-#       # Here we can do anything such as:
-#       # within release_path do
-#       #   execute :rake, 'cache:clear'
-#       # end
-#     end
-#   end
-
-# end
