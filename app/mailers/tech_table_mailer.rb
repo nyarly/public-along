84de1b4af95b6d74d763f19168cf495054ff13ea
Patricia Ho <pho@opentable.com>
2016-06-13 11:57:16 -0700

    add send-to add for alert email

--- app/mailers/tech_table_mailer.rb
+++ app/mailers/tech_table_mailer.rb
@@ -2,7 +2,7 @@ class TechTableMailer < ApplicationMailer
   default from: 'no-reply@opentable.com'
 
   def alert_email(message)
-    @email = "sampleTechTableEmail@opentable.com"
+    @email = "techtable@opentable.com"
     @message = message
     mail(to: @email, subject: "ALERT: Workday Integration Error")
   end
