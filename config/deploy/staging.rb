d06154b38f72d740e2665c5ffbcdf1cc71e368b2
Patricia Ho <pho@opentable.com>
2016-07-18 18:03:17 -0700

    change host name on staging deploy config

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
-server "workday-integration-pp-sf-01.otcorp.opentable.com", roles: [:web, :app, :db], primary: true #need a new mezzo hostname
+server "pp-sf-mezzo-01.otcorp.opentable.com", roles: [:web, :app, :db], primary: true
 
 set :deploy_to, "/var/www/mezzo"
 set :branch,    :staging
