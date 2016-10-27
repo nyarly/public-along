557cf6e43b181770ec974ce349581e544910f751
Patricia Ho <pho@opentable.com>
2016-10-27 14:29:36 -0700

    cap changes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server "mezzo-otcorp-sf-01.otcorp.opentable.com", roles: [:web, :app, :db], primary: true
+server "prod-sc-mezzo-01.otcorp.opentable.com", roles: [:web, :app, :db], primary: true
 
 set :deploy_to, "/var/www/mezzo"
 set :branch,    :production
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'mezzo.otenv.com' }
+  config.action_mailer.default_url_options = { host: 'mezzo.ot.tools' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'otcmail.otcorp.opentable.com',
