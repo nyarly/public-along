db460694d111279cbfab09c8ea70e5262e4e9a84
Patricia Ho <pho@opentable.com>
2017-03-08 17:32:30 -0800

    remove redundant rake task

--- config/schedule.rb
+++ config/schedule.rb
@@ -16,8 +16,6 @@ end
 
 every :weekday, at: TZInfo::Timezone.get("America/Los_Angeles").local_to_utc(Time.parse("18:00")) do
   rake "employee:offboard_report"
-  rake "db:saba:update_csvs"
-  rake "db:saba:sftp_drop"
 end
 
 # This is at UTC time so it runs at 6pm PST every day of the week except on Fridays
