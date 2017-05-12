c493569dd332e884d134a0ad739a110854dc915e
Paula Burke <pburke@t430cbraun.otcorp.opentable.com>
2017-05-11 19:01:37 -0700

    spec skeleton

--- spec/services/google_apps_services_spec.rb
+++ spec/services/google_apps_services_spec.rb
@@ -1,26 +1,34 @@
 require 'rails_helper'
 
 describe GoogleAppsService, type: :service do
-  let!(:employee) { FactoryGirl.create(:employee, termination_date: Date.new(2017, 6, 1)) }
-  let!(:emp_transaction) { FactoryGirl.create(:emp_transaction) }
-  let!(:offboarding_info) { FactoryGirl.create(:offboarding_info, emp_transction: emp_transaction.id, employee_id: employee.id, ) }
-  let!(:app_transaction) { FactoryGirl.create(:app_transaction, emp_transaction_id: emp_transaction.id, status: "Pending") }
+  let!(:employee) { FactoryGirl.create(:employee) }
 
-  Timecop.freeze(Time.new(2017, 6, 01, 15, 30, 0, "+00:00"))
+  describe "successfully transfers google app data" do
 
-  context "successfully transfers google app data" do
-    it "should get a success response from the google api" do
+    context "when the employee has offboarding info" do
+      let!(:emp_transaction) { FactoryGirl.create(:emp_transaction) }
+      let!(:offboarding_info) { FactoryGirl.create(:offboarding_info, emp_transction: emp_transaction.id, employee_id: employee.id, transfer_google_docs_id: 1) }
+
+      it "should get a success response from the google api" do
+        transfer_google = GoogleAppsService.new(employee)
+        expect(transfer_google).to eq(true)
+
+      end
     end
 
-    it "should update the app transaction status to 'success'" do
+    context "when the employee does not have offboarding info" do
     end
+
+
+    # it "should update the app transaction status to 'success'" do
+    # end
   end
 
-  context "fails to transfer google app data" do
-    it "should get a fail response from the google api" do
-    end
+  # context "fails to transfer google app data" do
+  #   it "should get a fail response from the google api" do
+  #   end
 
-    it "should update the app transaction to 'failed'" do
-    end
-  end
+  #   it "should update the app transaction to 'failed'" do
+  #   end
+  # end
 end
