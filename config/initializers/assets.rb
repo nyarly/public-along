bf18fd9da134d9f644dea2beb12c484d5161501a
Patricia Ho <pho@opentable.com>
2017-06-08 10:28:21 -0700

    move preview interceptor to a more reasonable place

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,6 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+# set preview interceptor to show images in preview mode
+ActionMailer::Base.register_preview_interceptor(ActionMailer::InlinePreviewInterceptor)
