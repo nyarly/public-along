ca1a77a56a7f291008085194e31b05f493d7a10c
Paula Burke <pburke@t430cbraun.otcorp.opentable.com>
2017-07-10 19:21:25 -0700

    add sftp drop code

--- app/services/betterworks_service.rb
+++ app/services/betterworks_service.rb
@@ -3,18 +3,70 @@ require 'net/sftp'
 
 class BetterworksService
 
-  def generate_csv
-    get_employees
 
+  # include employees with hire date less or equal to today
+  # include employees with termination date >= July 14, 2017
+  # include regular full-time and regular part-time employees only
+  def betterworks_users
+    active_emps = Employee.where("hire_date <= ? AND termination_date IS NULL", Date.today)
+    regular_active_emps = active_emps.joins(:worker_type).where(:worker_types => {:kind => "Regular"}).to_a
+    regular_active_emps
   end
 
-  # include regular full-time and regular part-time employees only
-  # employees with hire date less than today
-  # do not include any employee with a termination date < July 14, 2017
+  # betterworks recommends the following columns:
+  # "email", "first_name", "last_name", "department_name", "title", "manager_email", "deactivation_date"
+  # deactivation_date should be blank for current employees
+  def generate_employee_csv
+    dirname = "tmp/betterworks"
 
-  def get_employees
-    active_emps = Employee.where("hire_date <= ?", Date.today)
-    Employee.joins(:worker_type).where(:worker_types => {:kind => "Regular"})
+    # delete old file
+    Dir["tmp/betterworks/*"].each do |f|
+      File.delete(f)
+    end
+
+    unless File.directory?(dirnmae)
+      FileUtils.mkdir_p(dirname)
+    end
+
+    headers = [
+      "email",
+      "first_name",
+      "last_name",
+      "department_name",
+      "title",
+      "manager_email",
+      "deactivation_date"
+    ]
+
+    filename = "tmp/betterworks/OT_Betterworks_Users" + DateTime.now.strftime('%Y%m%d') + ".csv"
+
+    CSV.open(filename, "w+", {headers: true, col_sep: "|"}) do |csv|
+      csv << headers
+
+      betterworks_users.each do |u|
+        manager_email = u.manager ? u.manager.email : ""
+
+        csv << [
+          u.email,
+          u.first_name,
+          u.last_name,
+          u.department.name,
+          u.job_title.name,
+          manager_email,
+          u.termination_date
+        ]
+      end
+    end
+  end
+
+  def sftp_drop
+    uri = URI.parse("sftp://#{SECRETS.betterworks_host}")
+    Net::SFTP.start(uri.host, SECRETS.betterworks_user, password: SECRETS.betterworks_pw ) do |sftp|
+      # sftp.upload!("tmp/saba/OT_Betterworks_Users.csv", "/")
+      sftp.dir.foreach("/incoming") do |t|
+        puts t.inspect
+      end
+    end
   end
 
 end
--- spec/services/betterworks_service_spec.rb
+++ spec/services/betterworks_service_spec.rb
@@ -5,24 +5,58 @@ describe BetterworksService, type: :service do
   describe "create csv" do
 
     let(:service) { BetterworksService.new }
-    let(:ftr_worker_type) { FactoryGirl.create(:worker_type, code: "FTR", kind: "Regular")}
-    let(:ptr_worker_type) { FactoryGirl.create(:worker_type, code: "PTR", kind: "Regular")}
-    let(:temp_worker_type) { FactoryGirl.create(:worker_type, code: "FTT", kind: "Temporary")}
+    let(:ftr_worker_type) { FactoryGirl.create(:worker_type,
+      code: "FTR",
+      kind: "Regular")}
+    let(:ptr_worker_type) { FactoryGirl.create(:worker_type,
+      code: "PTR",
+      kind: "Regular")}
+    let(:temp_worker_type) { FactoryGirl.create(:worker_type,
+      code: "FTT",
+      kind: "Temporary")}
 
     it "should scope only regular employees" do
-      ftr_emp = FactoryGirl.create(:employee, worker_type: ftr_worker_type)
-      ptr_emp = FactoryGirl.create(:employee, worker_type: ptr_worker_type)
-      temp = FactoryGirl.create(:employee, worker_type: temp_worker_type)
+      ftr_emp = FactoryGirl.create(:employee,
+        worker_type: ftr_worker_type,
+        hire_date: Date.new(2017, 1, 1))
+      ptr_emp = FactoryGirl.create(:employee,
+        worker_type: ptr_worker_type,
+        hire_date: Date.new(2017, 1, 1))
+      temp = FactoryGirl.create(:employee,
+        worker_type: temp_worker_type,
+        hire_date: Date.new(2017, 1, 1))
 
-      expect(service.get_employees.count).to eq(2)
-      expect(service.get_employees).to include(ftr_emp)
-      expect(service.get_employees).to include(ptr_emp)
-      expect(service.get_employees).not_to include(temp)
+      expect(service.betterworks_users.count).to eq(2)
+      expect(service.betterworks_users).to include(ftr_emp)
+      expect(service.betterworks_users).to include(ptr_emp)
+      expect(service.betterworks_users).not_to include(temp)
     end
 
-    it "should not include any employee with a termination date" do
-      ftr_emp = FactoryGirl.create(:employee, worker_type: ftr_worker_type)
+    it "should not include employees who have not started" do
+      ftr_emp = FactoryGirl.create(:employee,
+        hire_date: Date.new(2017, 1, 1),
+        worker_type: ftr_worker_type)
+      ftr_new_emp = FactoryGirl.create(:employee,
+        worker_type: ftr_worker_type,
+        hire_date: Date.today + 2.weeks)
 
+      expect(service.betterworks_users.count).to eq(1)
+      expect(service.betterworks_users).to include(ftr_emp)
+      expect(service.betterworks_users).not_to include(ftr_new_emp)
+    end
+
+    it "should not include terminated employees" do
+      ftr_emp = FactoryGirl.create(:employee,
+        hire_date: Date.new(2017, 1, 1),
+        worker_type: ftr_worker_type)
+      ftr_termed_emp = FactoryGirl.create(:employee,
+        worker_type: ftr_worker_type,
+        hire_date: Date.new(2017, 1, 1),
+        termination_date: Date.new(2017, 5, 4))
+
+      expect(service.betterworks_users.count).to eq(1)
+      expect(service.betterworks_users).to include(ftr_emp)
+      expect(service.betterworks_users).not_to include(ftr_termed_emp)
     end
   end
 
