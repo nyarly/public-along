8f9b40422cb098f78c8733a239483e3ca92789ec
Paula Burke <pburke@t430cbraun.otcorp.opentable.com>
2017-07-27 17:57:34 -0700

    specs

--- app/services/audit_service.rb
+++ app/services/audit_service.rb
@@ -1,43 +1,39 @@
 class AuditService
 
   def check_for_missed_terminations
-    employees_to_audit = []
+    audit = []
 
     # scenario: worker has termination date in past but is still active
-    missed_offboards = Employee.where('termination_date < ? AND status LIKE ?', 2.days.ago, "Active")
+    missed_offboards = Employee.where('termination_date < ? AND status LIKE ?', 2.days.ago, "Active").to_a
     # scenario: worker has not been updated in adp sync recently and is active
-    missed_terminations = Employee.where('updated_at < ? AND status LIKE ?', 2.days.ago, "Active")
+    missed_terminations = Employee.where('updated_at < ? AND status LIKE ?', 2.days.ago, "Active").to_a
     # scenario: worker's contract has ended and they were not given a termination date
-    contract_ended = Employee.where('contract_end_date < ? AND status LIKE ? AND termination_date IS NULL', 2.days.ago, "Active")
+    contract_ended = Employee.where('contract_end_date < ? AND status LIKE ? AND termination_date IS NULL', 2.days.ago, "Active").to_a
 
+    employees_to_audit = missed_offboards + missed_terminations + contract_ended
 
-
-    missed_offboards.each do |employee|
-      employee_record = {}
+    employees_to_audit.uniq.each do |employee|
       adp_status = get_adp_status(employee)
+      employee_hash = generate_employee_hash(employee, adp_status)
 
-      if status == "Terminated"
-        terminate(employee)
-      end
+      audit << employee_hash
+    end unless employees_to_audit.empty?
 
-      employee_record['name'] = employee.cn
-      employee_record['department'] = employee.department.name
-      employee_record['location'] = employee.location.name
-      employee_record['job_title'] = employee.job_title.name
-      employee_record['manager'] = employee.manager_id.present? ? "#{employee.manager.first_name} #{employee.manager.last_name}" : ""
-      employee_record['adp_status'] = adp_status
-      employee_record['mezzo_status'] = employee.status
-      employee_record['termination_date'] = employee.termination_date.present? ? employee.termination_date : ""
-      employee_record['contract_end_date'] = employee.contract_end_date.present? ? employee.contract_end_date : ""
-
-      employees_to_audit << employee_record
-    end
-
-    employees_to_audit
+    audit
   end
 
+  def confirm_ad_deactivation
+    terminated_employees = Employee.where(status: "Terminated")
+    ads = ActiveDirectoryService.new
 
-  # check that terminated workers are disabled in active directory
+    terminated_employees.all.each do |t|
+      ad_entry = ads.find_entry("sAMAccountName", e.sam_account_name).first
+
+      if ad_entry.dn.include? "OU=Disabled Users" and ad_entry.userAccountControl == ["514"]
+        # do the things
+      end
+    end
+  end
 
   # check for workers with contract end date in past but no termination date
     # check disabled and email p&c
@@ -47,28 +43,34 @@ class AuditService
     # inactive workers should be disabled in active directory
 
 
-  def get_adp_status(employee)
-    status = "Inconclusive"
+  private
 
+  def get_adp_status(employee)
+    status = "not found"
     adp = AdpService::Base.new
     json = adp.worker("/#{employee.adp_assoc_oid}")
 
     if json["workers"].present?
-      w_hash = workers[0]
       status = json.dig("workers", 0, "workerStatus", "statusCode", "codeValue")
     end
 
     status
   end
 
-  def terminate(employee)
-    ads = ActiveDirectoryService.new
-    ads.deactivate([e])
+  def employee_hash(employee, adp_status)
+    employee_record = {}
 
-    TechTableMailer.offboard_notice(employee).deliver_now
+    employee_record['name'] = employee.cn
+    employee_record['department'] = employee.department.name
+    employee_record['location'] = employee.location.name
+    employee_record['job_title'] = employee.job_title.name
+    employee_record['manager'] = employee.manager_id.present? ? "#{employee.manager.first_name} #{employee.manager.last_name}" : ""
+    employee_record['adp_status'] = adp_status
+    employee_record['mezzo_status'] = employee.status
+    employee_record['termination_date'] = employee.termination_date.present? ? employee.termination_date : ""
+    employee_record['contract_end_date'] = employee.contract_end_date.present? ? employee.contract_end_date : ""
 
-    off = OffboardingService.new
-    off.offboard([e])
+    employee_record
   end
 
 end
--- spec/services/audit_service_spec.rb
+++ spec/services/audit_service_spec.rb
@@ -2,33 +2,58 @@ require 'rails_helper'
 
 describe AuditService, type: :service do
 
-  let(:regular_employee) { FactoryGirl.create(:employee,
-    status: "Active")}
-  let(:regular_termination) { FactoryGirl.create(:employee,
+  let(:audit_service) { AuditService.new }
+  let(:adp_service) { double(AdpService::Base) }
+  let(:regular_worker_type) { FactoryGirl.create(:worker_type)}
+
+  let!(:manager) { FactoryGirl.create(:employee) }
+  let!(:regular_employee) { FactoryGirl.create(:employee,
+    manager_id: manager.employee_id,
+    status: "Active",
+    worker_type_id: regular_worker_type.id)}
+  let!(:regular_termination) { FactoryGirl.create(:employee,
+    manager_id: manager.employee_id,
     status: "Terminated",
-    termination_date: 1.week.ago)}
-  let(:missed_offboard) { FactoryGirl.create(:employee,
+    termination_date: 1.week.ago,
+    worker_type_id: regular_worker_type.id)}
+  let!(:missed_offboard) { FactoryGirl.create(:employee,
+    manager_id: manager.employee_id,
     status: "Active",
-    termination_date: 3.days.ago)}
-  let(:missed_termination) { FactoryGirl.create(:employee,
+    termination_date: 3.days.ago,
+    worker_type_id: regular_worker_type.id)}
+  let!(:missed_termination) { FactoryGirl.create(:employee,
+    manager_id: manager.employee_id,
     status: "Active",
-    updated_at: 3.days.ago)}
-  let(:contract_ended) { FactoryGirl.create(:employee,
+    updated_at: 3.days.ago,
+    worker_type_id: regular_worker_type.id)}
+  let!(:missed_contract_end) { FactoryGirl.create(:employee,
+    manager_id: manager.employee_id,
     status: "Active",
-    worker_type: WorkerType.find_by(kind: "Contractor"),
+    worker_type_id: regular_worker_type.id,
     contract_end_date: 1.week.ago)}
+  let!(:terminated_worker_json) { File.read(Rails.root.to_s+"/spec/fixtures/adp_termed_worker.json")}
+
+  before :each do
+    allow(AdpService::Base).to receive(:new).and_return(adp_service)
+  end
 
   context "when running an audit" do
-    it "should find the missed termination" do
+
+    it "should find the missed offboards" do
+      allow(adp_service).to receive(:worker).and_return(JSON.parse(terminated_worker_json))
+      missing_terminations = audit_service.check_for_missed_terminations
+      expect(missing_terminations.length).to eq(3)
+      expect(missing_terminations.any? { |hash| hash["name"] == "#{missed_termination.cn}"}).to eq(true)
+      expect(missing_terminations.any? { |hash| hash["name"] == "#{missed_offboard.cn}"}).to eq(true)
+      expect(missing_terminations.any? { |hash| hash["name"] == "#{missed_contract_end.cn}"}).to eq(true)
+      expect(missing_terminations.any? { |hash| hash["name"] == "#{regular_termination.cn}"}).to eq(false)
+      expect(missing_terminations.any? { |hash| hash["name"] == "#{regular_employee.cn}"}).to eq(false)
     end
 
-    it "should find the missed offboard" do
-    end
-
-    it "should find the missed contract end" do
-    end
-
-    it "should return an array with the missed hashes" do
+    it "should check the adp status" do
+      expect(adp_service).to receive(:worker).and_return(JSON.parse(terminated_worker_json)).thrice
+      missing_terminations = audit_service.check_for_missed_terminations
+      expect(missing_terminations.any? { |hash| hash["adp_status"] == "Terminated"}).to eq(true)
     end
   end
 
