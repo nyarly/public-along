285222ecbfe138a096ca7da16ef3ba74e00b0ad7
Paula Burke <pburke@t430cbraun.otcorp.opentable.com>
2017-07-31 12:58:20 -0700

    fix email address

--- app/mailers/summary_report_mailer.rb
+++ app/mailers/summary_report_mailer.rb
@@ -26,9 +26,8 @@ class SummaryReportMailer < ApplicationMailer
   def termination_audit_report
     audit = AuditService.new
     missed_terminations = audit.missed_terminations
-
     attachments.inline["audit_summary_#{DateTime.now}.csv"] = audit.generate_csv(missed_terminations)
-    mail(to: "pburke@opentable.com", subject: "Mezzo Missed Termination Audit")
+    mail(to: Rails.application.secrets.tt_email, subject: "Mezzo Missed Termination Audit")
   end
 
   def deactivation_audit_report
