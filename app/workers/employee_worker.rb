ca0ce0746665e5ce10525269556557af015787e0
Paula Burke <pburke@t430cbraun.otcorp.opentable.com>
2017-09-11 10:32:06 -0700

    update sidekiq worker params format

--- app/workers/employee_worker.rb
+++ app/workers/employee_worker.rb
@@ -3,14 +3,17 @@ class EmployeeWorker
 
   def perform(action, opts={})
     logger.info "Performing onboarding manager mailer"
-    if opts[:employee_id].present?
-      @employee = Employee.find(opts[:employee_id])
+    logger.info action
+    logger.info opts
+    puts opts
+    if opts["employee_id"].present?
+      @employee = Employee.find(opts["employee_id"])
       @manager = @employee.manager
       @mailer = ManagerMailer.permissions(action, @manager, @employee) if @manager.present?
       logger.info "on hire event for @employee.cn, sending to @manager.email"
-    elsif opts[:event_id].present?
+    elsif opts["event_id"].present?
       profiler = EmployeeProfile.new
-      @event = AdpEvent.find(opts[:event_id])
+      @event = AdpEvent.find(opts["event_id"])
       @employee = profiler.build_employee(@event)
       @manager = @employee.manager
       @mailer = ManagerMailer.permissions(action, @manager, @employee, event: @event) if @manager.present?
