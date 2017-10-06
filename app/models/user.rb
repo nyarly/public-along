2da2739778f1d201975b5722648e8a1b98af5b4d
Paula Burke <pburke@opentable.com>
2017-10-05 18:38:57 -0700

    add activity feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,4 +28,8 @@ class User < ActiveRecord::Base
     self.last_name = Devise::LDAP::Adapter.get_ldap_param(self.ldap_user,"sn").first
     self.employee_id = Devise::LDAP::Adapter.get_ldap_param(self.ldap_user,"employeeID").first
   end
+
+  def full_name
+    self.first_name + " " + self.last_name
+  end
 end
