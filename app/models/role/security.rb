c93a1e26201b2d6d0c14cd88c53c633ebd94da80
Paula Burke <pburke@opentable.com>
2017-12-11 11:34:13 -0800

    map security role from security group

--- app/models/role/security.rb
+++ app/models/role/security.rb
@@ -5,18 +5,6 @@ class Role::Security < Role
     ability.can :read, Employee
     ability.can :direct_reports, Employee
     ability.can :autocomplete_name, Employee
-    ability.can :read, MachineBundle
-    ability.can :read, Location
-    ability.can :read, Department
-    ability.can :read, DeptSecProf
-    ability.can :read, SecurityProfile
-    ability.can :read, SecProfAccessLevel
-    ability.can :read, AccessLevel
-    ability.can :read, Application
-    ability.can :read, EmpTransaction
-    ability.can :read, EmpSecProfile
-    ability.can :read, ParentOrg
-    ability.can :read, WorkerType
     ability.can :read, Profile
   end
 end
--- spec/models/role/security_spec.rb
+++ spec/models/role/security_spec.rb
@@ -6,18 +6,6 @@ describe Role::Security, type: :model do
 
   describe 'abilities' do
     it_should_behave_like 'role abilities', Employee, [:read, :direct_reports, :autocomplete_name]
-    it_should_behave_like 'role abilities', MachineBundle, [:read]
-    it_should_behave_like 'role abilities', Location, [:read]
-    it_should_behave_like 'role abilities', Department, [:read]
-    it_should_behave_like 'role abilities', DeptSecProf, [:read]
-    it_should_behave_like 'role abilities', SecurityProfile, [:read]
-    it_should_behave_like 'role abilities', SecProfAccessLevel, [:read]
-    it_should_behave_like 'role abilities', AccessLevel, [:read]
-    it_should_behave_like 'role abilities', Application, [:read]
-    it_should_behave_like 'role abilities', EmpTransaction, [:read]
-    it_should_behave_like 'role abilities', EmpSecProfile, [:read]
-    it_should_behave_like 'role abilities', ParentOrg, [:read]
-    it_should_behave_like 'role abilities', WorkerType, [:read]
     it_should_behave_like 'role abilities', Profile, [:read]
   end
 end
