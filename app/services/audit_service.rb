2c853eff1098e2258c367e0b7ea742031f7d300c
Paula Burke <pburke@opentable.com>
2018-04-09 18:19:16 -0700

    update audit report

--- app/services/audit_service.rb
+++ app/services/audit_service.rb
@@ -2,49 +2,55 @@ require 'csv'
 
 class AuditService
 
+  def initialize
+    @ad_service ||= ad_service
+  end
+
   # scenario: worker has termination date in past but is still active
   # scenario: worker has not been updated in adp sync recently and is active
   # scenario: worker's contract has ended and they were not given a termination date
   def missed_terminations
     audit = []
-    missed_offboards = Employee.where('termination_date < ? AND status LIKE ?', 2.days.ago, "active").to_a
-    missed_terminations = Employee.where('updated_at < ? AND status LIKE ?', 2.days.ago, "active").to_a
-    contract_ended = Employee.where('contract_end_date < ? AND status LIKE ? AND termination_date IS NULL', 2.days.ago, "active").to_a
-
-    employees_to_audit = missed_offboards + missed_terminations + contract_ended
-    employees_to_audit.uniq.each do |employee|
+    audit_list.each do |employee|
       data = check_adp(employee)
       employee_hash = generate_employee_hash(employee, data)
       audit << employee_hash
-    end unless employees_to_audit.empty?
+    end unless audit_list.empty?
     audit
   end
 
-  def ad_deactivation
+  def audit_list
+    Employee.where('(status LIKE ?
+                    OR offboarded_at IS NULL)
+                    AND (termination_date < ?
+                    OR adp_status LIKE ?
+                    OR updated_at < ?
+                    OR contract_end_date < ?)',
+                    'active',
+                    1.day.ago,
+                    'Terminated',
+                    2.hours.ago,
+                    1.day.ago)
+  end
+
+  def missed_deactivations
     audit = []
-    terminated_employees = Employee.where(status: "terminated")
-    ads = ActiveDirectoryService.new
 
     terminated_employees.each do |t|
-      ad_entry = ads.find_entry("sAMAccountName", t.sam_account_name).first
-      if ad_entry.present?
-        ldap_dn = ad_entry[:dn][0].downcase
-        usr_acct_ctrl = ad_entry[:useraccountcontrol][0]
-        if ldap_dn != t.dn.downcase and usr_acct_ctrl != "514"
-          employee_hash = generate_employee_hash(t, {'ldap_dn'=>ldap_dn})
-          audit << employee_hash
-        end
+      ad_entry = check_active_directory(t)
+      if ad_entry.present? && deactivated?(ad_entry, t)
+        audit << generate_employee_hash(t, {'ldap_dn'=>ldap_dn(ad_entry)})
       end
     end
     audit
   end
 
   def generate_csv(data)
-    CSV.generate(headers: true, col_sep: ",") do |csv|
+    CSV.generate(headers: true) do |csv|
       csv << data.first.keys
       data.each do |e|
         row = []
-        e.each do |k,v|
+        e.each do |_,v|
           row << v
         end
         csv << row
@@ -54,12 +60,36 @@ class AuditService
 
   private
 
+  def terminated_employees
+    Employee.where(status: "terminated")
+  end
+
+  def ad_service
+    @ad_service ||= ActiveDirectoryService.new
+  end
+
+  def check_active_directory(t)
+    ad_service.find_entry("sAMAccountName", t.sam_account_name).first
+  end
+
+  def ldap_dn(ad_entry)
+    ad_entry[:dn][0].downcase
+  end
+
+  def usr_acct_ctrl(ad_entry)
+    ad_entry[:useraccountcontrol][0]
+  end
+
+  def deactivated?(ad_entry, worker)
+    ldap_dn(ad_entry) != worker.dn.downcase || usr_acct_ctrl(ad_entry) != '514'
+  end
+
   def check_adp(e)
     data = {}
     adp = AdpService::Base.new
     json = adp.worker("/#{e.adp_assoc_oid}")
     if json["workers"].present?
-      data[:adp_status] = json.dig("workers", 0, "workerStatus", "statusCode", "codeValue")
+      data[:current_adp_status] = json.dig("workers", 0, "workerStatus", "statusCode", "codeValue")
       data[:adp_term_date] = json.dig("workers", 0, "workerDates", "terminationDate")
     end
     data
@@ -72,9 +102,11 @@ class AuditService
     h[:department] = e.department.name
     h[:location] = e.location.name
     h[:manager] = e.manager_id.present? ? "#{e.manager.first_name} #{e.manager.last_name}" : ""
-    h[:mezzo_status] = e.status
-    h[:mezzo_term_date] = e.termination_date.present? ? e.termination_date.strftime("%Y-%m-%d") : ""
+    h[:status] = e.status
+    h[:adp_status] = e.adp_status
+    h[:term_date] = e.termination_date.present? ? e.termination_date.strftime("%Y-%m-%d") : ""
     h[:contract_end_date] = e.contract_end_date.present? ? e.contract_end_date.strftime("%Y-%m-%d") : ""
+    h[:offboarded_at] = e.offboarded_at.present? ? e.offboarded_at.strftime("%Y-%m-%d") : ""
     opts.each do |k,v|
       h[k] = v
     end unless opts.empty?
--- spec/services/audit_service_spec.rb
+++ spec/services/audit_service_spec.rb
@@ -2,109 +2,154 @@ require 'rails_helper'
 
 describe AuditService, type: :service do
 
+  let!(:json)         { File.read(Rails.root.to_s + '/spec/fixtures/adp_termed_worker.json') }
   let(:audit_service) { AuditService.new }
-  let(:adp_service) { double(AdpService::Base) }
-  let(:ad_service) { double(ActiveDirectoryService) }
+  let(:adp_service)   { double(AdpService::Base) }
+  let(:ad)            { double(ActiveDirectoryService) }
+  let!(:manager)      { FactoryGirl.create(:employee) }
+  let!(:employee)     { FactoryGirl.create(:active_employee, manager: manager) }
 
-  let!(:manager) { FactoryGirl.create(:employee) }
-  let!(:regular_employee) { FactoryGirl.create(:active_employee,
-    manager: manager) }
-  let!(:regular_termination) { FactoryGirl.create(:terminated_employee,
-    first_name: "Diane",
-    last_name: "Sawyer",
-    termination_date: 1.week.ago,
-    sam_account_name: "dsawyer",
-    manager: manager) }
-  let!(:missed_deactivation) { FactoryGirl.create(:terminated_employee,
-    first_name: "Tom",
-    last_name: "Brokaw",
-    sam_account_name: "tbrowkow",
-    termination_date: 3.days.ago,
-    manager: manager) }
-  let!(:missed_offboard) { FactoryGirl.create(:active_employee,
-    termination_date: 4.days.ago,
-    manager: manager) }
-  let!(:missed_termination) { FactoryGirl.create(:active_employee, :existing,
-    updated_at: 3.days.ago,
-    termination_date: nil,
-    manager: manager) }
-  let!(:missed_contract_end) { FactoryGirl.create(:active_employee,
-    contract_end_date: 1.week.ago,
-    manager: manager) }
-
-  let!(:terminated_worker_json) { File.read(Rails.root.to_s+"/spec/fixtures/adp_termed_worker.json")}
-
-  before :each do
-    allow(AdpService::Base).to receive(:new).and_return(adp_service)
-    allow(ActiveDirectoryService).to receive(:new).and_return(ad_service)
+  let!(:regular_termination) do
+    FactoryGirl.create(:terminated_employee,
+      first_name: 'Diane',
+      last_name: 'Sawyer',
+      termination_date: 1.week.ago,
+      offboarded_at: 1.week.ago,
+      sam_account_name: 'dsawyer',
+      manager: manager)
+  end
+  let!(:missed_deactivation) do
+    FactoryGirl.create(:terminated_employee,
+      first_name: 'Tom',
+      last_name: 'Brokaw',
+      sam_account_name: 'tbrowkow',
+      termination_date: 3.days.ago,
+      offboarded_at: 3.days.ago,
+      manager: manager,
+      adp_status: 'Terminated')
+  end
+  let!(:missed_offboard) do
+    FactoryGirl.create(:active_employee,
+      last_name: 'A',
+      status: 'active',
+      adp_status: 'Active',
+      termination_date: 1.day.ago,
+      manager: manager)
+  end
+  let!(:missed_offboard_2) do
+    FactoryGirl.create(:active_employee,
+      last_name: 'B',
+      termination_date: Date.today,
+      manager: manager,
+      status: 'active',
+      adp_status: 'Terminated')
+  end
+  let!(:missed_offboard_3) do
+    FactoryGirl.create(:terminated_employee,
+      last_name: 'C',
+      termination_date: Date.yesterday,
+      sam_account_name: 'cc',
+      offboarded_at: nil,
+      manager: manager,
+      adp_status: 'Terminated')
+  end
+  let!(:missed_termination) do
+    FactoryGirl.create(:active_employee, :existing,
+      last_name: 'D',
+      updated_at: 3.days.ago,
+      termination_date: nil,
+      manager: manager)
+  end
+  let!(:missed_contract_end) do
+    FactoryGirl.create(:active_employee,
+      last_name: 'E',
+      contract_end_date: 1.week.ago,
+      manager: manager)
   end
 
-  context "checking for missed terminations" do
-    let(:term_csv) {
+  describe '#missed_terminations' do
+    subject(:audit) { audit_service.missed_terminations }
+
+    let(:csv) { audit_service.generate_csv(audit) }
+    let(:term_csv) do
       <<-EOS.strip_heredoc
-      name,job_title,department,location,manager,mezzo_status,mezzo_term_date,contract_end_date,adp_status,adp_term_date
-      #{missed_offboard.cn},#{missed_offboard.job_title.name},#{missed_offboard.department.name},#{missed_offboard.location.name},#{missed_offboard.manager.first_name} #{missed_offboard.manager.last_name},active,#{missed_offboard.termination_date.strftime('%Y-%m-%d')},"",Terminated,2017-06-01
-      #{missed_termination.cn},#{missed_termination.job_title.name},#{missed_termination.department.name},#{missed_termination.location.name},#{missed_termination.manager.first_name} #{missed_termination.manager.last_name},active,"","",Terminated,2017-06-01
-      #{missed_contract_end.cn},#{missed_contract_end.job_title.name},#{missed_contract_end.department.name},#{missed_contract_end.location.name},#{missed_contract_end.manager.first_name} #{missed_contract_end.manager.last_name},active,"",#{missed_contract_end.contract_end_date.strftime('%Y-%m-%d')},Terminated,2017-06-01
+      name,job_title,department,location,manager,status,adp_status,term_date,contract_end_date,offboarded_at,current_adp_status,adp_term_date
+      #{missed_offboard.cn},#{missed_offboard.job_title.name},#{missed_offboard.department.name},#{missed_offboard.location.name},#{missed_offboard.manager.cn},active,Active,#{missed_offboard.termination_date.strftime('%Y-%m-%d')},"","",Terminated,2017-06-01
+      #{missed_offboard_2.cn},#{missed_offboard_2.job_title.name},#{missed_offboard_2.department.name},#{missed_offboard_2.location.name},#{missed_offboard_2.manager.cn},active,Terminated,#{missed_offboard_2.termination_date.strftime('%Y-%m-%d')},"","",Terminated,2017-06-01
+      #{missed_offboard_3.cn},#{missed_offboard_3.job_title.name},#{missed_offboard_3.department.name},#{missed_offboard_3.location.name},#{missed_offboard_3.manager.cn},terminated,Terminated,#{Date.yesterday.strftime('%Y-%m-%d')},"","",Terminated,2017-06-01
+      #{missed_termination.cn},#{missed_termination.job_title.name},#{missed_termination.department.name},#{missed_termination.location.name},#{missed_termination.manager.cn},active,,"","","",Terminated,2017-06-01
+      #{missed_contract_end.cn},#{missed_contract_end.job_title.name},#{missed_contract_end.department.name},#{missed_contract_end.location.name},#{missed_contract_end.manager.cn},active,,"",#{missed_contract_end.contract_end_date.strftime('%Y-%m-%d')},"",Terminated,2017-06-01
       EOS
-    }
-
-    it "should find the missed offboards" do
-      allow(adp_service).to receive(:worker).and_return(JSON.parse(terminated_worker_json))
-      missing_terminations = audit_service.missed_terminations
-
-      expect(missing_terminations.length).to eq(3)
-      expect(missing_terminations.any? { |hash| hash[:name] == "#{missed_termination.cn}"}).to eq(true)
-      expect(missing_terminations.any? { |hash| hash[:name] == "#{missed_offboard.cn}"}).to eq(true)
-      expect(missing_terminations.any? { |hash| hash[:name] == "#{missed_contract_end.cn}"}).to eq(true)
-      expect(missing_terminations.any? { |hash| hash[:name] == "#{regular_termination.cn}"}).to eq(false)
-      expect(missing_terminations.any? { |hash| hash[:name] == "#{regular_employee.cn}"}).to eq(false)
     end
 
-    it "should check the adp status" do
-      expect(adp_service).to receive(:worker).and_return(JSON.parse(terminated_worker_json)).thrice
-      missing_terminations = audit_service.missed_terminations
-      expect(missing_terminations.any? { |hash| hash[:adp_status] == "Terminated"}).to eq(true)
-      expect(missing_terminations.any? { |hash| hash[:adp_term_date] == "2017-06-01"}).to eq(true)
+    before do
+      allow(adp_service).to receive(:worker).and_return(JSON.parse(json))
+      allow(AdpService::Base).to receive(:new).and_return(adp_service)
+      allow(ActiveDirectoryService).to receive(:new).and_return(ad)
+    end
+
+    it 'has all the missed terminations' do
+      expect(audit.length).to eq(5)
+    end
+
+    it 'checks the adp status' do
+      audit_service.missed_terminations
+      expect(adp_service).to have_received(:worker).exactly(5).times
+    end
+
+    it 'gets the adp status' do
+      expect(audit.any? { |hash| hash[:adp_status] == 'Terminated' }).to eq(true)
     end
 
     it "should output csv string for missed offboards" do
-      allow(adp_service).to receive(:worker).and_return(JSON.parse(terminated_worker_json))
-      missed_terminations = audit_service.missed_terminations
-      generated_csv = audit_service.generate_csv(missed_terminations)
-      expect(generated_csv).to eq(term_csv)
+      expect(csv).to eq(term_csv)
     end
   end
 
-  context "confirming AD deactivation for terminated users" do
-    let(:disabled_ldap_entry) do [{
-      :dn=>["CN=Diane Sawyer,OU=Disabled Users,OU=Users,OU=OT,DC=ottest,DC=opentable,DC=com"],
-      :useraccountcontrol=>["514"]}] end
-    let(:enabled_ldap_entry) do [{
-      :dn=>["CN=Tom Browkaw,OU=IT,OU=Users,OU=OT,DC=ottest,DC=opentable,DC=com"],
-      :useraccountcontrol=>["512"]}] end
-    let(:ad_csv) {
-      <<-EOS.strip_heredoc
-      name,job_title,department,location,manager,mezzo_status,mezzo_term_date,contract_end_date,ldap_dn
-      #{missed_deactivation.cn},#{missed_deactivation.job_title.name},#{missed_deactivation.department.name},#{missed_deactivation.location.name},#{missed_deactivation.manager.first_name} #{missed_deactivation.manager.last_name},terminated,#{missed_deactivation.termination_date.strftime('%Y-%m-%d')},"","cn=tom browkaw,ou=it,ou=users,ou=ot,dc=ottest,dc=opentable,dc=com"
-      EOS
-    }
+  describe '#missed_deactivation' do
+    subject(:audit) { audit_service.missed_deactivations }
 
-    it "should should check all terminated users" do
-      expect(ad_service).to receive(:find_entry).with("sAMAccountName", regular_termination.sam_account_name).and_return(disabled_ldap_entry)
-      expect(ad_service).to receive(:find_entry).with("sAMAccountName", missed_deactivation.sam_account_name).and_return(enabled_ldap_entry)
-      missed_deactivations = audit_service.ad_deactivation
-      expect(missed_deactivations.length).to eq(1)
-      expect(missed_deactivations.any? { |hash| hash[:name] == "#{missed_deactivation.cn}"}).to eq(true)
-      expect(missed_deactivations.any? { |hash| hash[:name] == "#{regular_termination.cn}"}).to eq(false)
+    let(:csv) { audit_service.generate_csv(audit) }
+
+    let(:disabled_ldap_entry) do
+      [{ :dn=>["CN=Diane Sawyer,OU=Disabled Users,OU=OT,DC=ottest,DC=opentable,DC=com"],
+         :useraccountcontrol=>["514"]}]
     end
 
-    it "should output csv string for missed AD deactivations" do
-      expect(ad_service).to receive(:find_entry).with("sAMAccountName", regular_termination.sam_account_name).and_return(disabled_ldap_entry)
-      expect(ad_service).to receive(:find_entry).with("sAMAccountName", missed_deactivation.sam_account_name).and_return(enabled_ldap_entry)
-      missed_ad_deactivations = audit_service.ad_deactivation
-      generated_csv = audit_service.generate_csv(missed_ad_deactivations)
-      expect(generated_csv).to eq(ad_csv)
+    let(:disabled_ldap_entry_2) do
+      [{ :dn=>["CN=#{missed_offboard_3.cn},OU=IT,OU=Users,OU=OT,DC=ottest,DC=opentable,DC=com"],
+         :useraccountcontrol=>["514"]}]
+    end
+
+    let(:enabled_ldap_entry) do
+      [{ :dn=>["CN=Tom Browkaw,OU=IT,OU=Users,OU=OT,DC=ottest,DC=opentable,DC=com"],
+         :useraccountcontrol=>["512"]}]
+    end
+
+    let(:ad_csv) do
+      <<-EOS.strip_heredoc
+      name,job_title,department,location,manager,status,adp_status,term_date,contract_end_date,offboarded_at,ldap_dn
+      #{missed_deactivation.cn},#{missed_deactivation.job_title.name},#{missed_deactivation.department.name},#{missed_deactivation.location.name},#{missed_deactivation.manager.cn},terminated,Terminated,#{missed_deactivation.termination_date.strftime('%Y-%m-%d')},"",#{missed_deactivation.offboarded_at.strftime('%Y-%m-%d')},"cn=tom browkaw,ou=it,ou=users,ou=ot,dc=ottest,dc=opentable,dc=com"
+      #{missed_offboard_3.cn},#{missed_offboard_3.job_title.name},#{missed_offboard_3.department.name},#{missed_offboard_3.location.name},#{missed_offboard_3.manager.cn},terminated,Terminated,#{missed_offboard_3.termination_date.strftime('%Y-%m-%d')},"","","cn=#{missed_offboard_3.cn.downcase},ou=it,ou=users,ou=ot,dc=ottest,dc=opentable,dc=com"
+      EOS
+    end
+
+    before do
+      allow(ActiveDirectoryService).to receive(:new).and_return(ad)
+      allow(ad).to receive(:find_entry).with("sAMAccountName", regular_termination.sam_account_name).and_return(disabled_ldap_entry)
+      allow(ad).to receive(:find_entry).with("sAMAccountName", missed_offboard_3.sam_account_name).and_return(disabled_ldap_entry_2)
+      allow(ad).to receive(:find_entry).with("sAMAccountName", missed_deactivation.sam_account_name).and_return(enabled_ldap_entry)
+      audit_service.missed_deactivations
+    end
+
+    it 'checks AD deactivation for each terminated worker' do
+      expect(ad).to have_received(:find_entry).with("sAMAccountName", regular_termination.sam_account_name)
+      expect(ad).to have_received(:find_entry).with("sAMAccountName", missed_deactivation.sam_account_name)
+      expect(ad).to have_received(:find_entry).with("sAMAccountName", missed_offboard_3.sam_account_name)
+    end
+
+    it 'outputs csv with missed deactivation' do
+      expect(csv).to eq(ad_csv)
     end
   end
 end
