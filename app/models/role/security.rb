087a26044c21b6fbb011895c532d68e8d33c36be
Paula Burke <pburke@opentable.com>
2018-06-12 11:20:01 -0700

    give security role access to additional employee views

--- app/models/role/security.rb
+++ app/models/role/security.rb
@@ -3,10 +3,11 @@ class Role::Security < Role
 
   def set_abilities(ability)
     ability.can :read, Employee
-    ability.can :direct_reports, Employee.all do |e|
-      e.id == user.employee_id || ManagementTreeQuery.new(e).up.include?(user.employee_id)
-    end
+    ability.can :direct_reports, Employee
     ability.can :autocomplete_name, Employee
     ability.can :read, Profile
+    ability.can :read, :new_hire
+    ability.can :read, :offboard
+    ability.can :read, :inactive
   end
 end
--- spec/models/role/security_spec.rb
+++ spec/models/role/security_spec.rb
@@ -7,5 +7,8 @@ describe Role::Security, type: :model do
   describe 'abilities' do
     it_should_behave_like 'role abilities', Employee, [:read, :direct_reports, :autocomplete_name]
     it_should_behave_like 'role abilities', Profile, [:read]
+    it_should_behave_like 'role abilities', :new_hire, [:read]
+    it_should_behave_like 'role abilities', :offboard, [:read]
+    it_should_behave_like 'role abilities', :inactive, [:read]
   end
 end
